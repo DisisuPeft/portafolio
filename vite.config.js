aa35f2dfb500a76a6d661954eb03e0be5d3ca530
Daniel Fernández <defp24099@gmail.com>
2024-09-29 15:38:41 -0600

    commit 11 290924

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    // Directorio de salida (por defecto es 'dist')
-    outDir: "dist",
-    // Genera un manifest.json (opcional)
-    manifest: false,
-    rollupOptions: {
-      // Ruta de entrada relativa correcta
-      input: "/src/main.js",
-    },
-  },
-  // Base pública, ajusta si despliegas en un subdirectorio
-  base: "/",
 });
